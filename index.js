6ce49b0d5f9dffe946765210f5058860d6329f00
vaxtale2023 <122852786+vaxtale2023@users.noreply.github.com>
2023-12-21 19:07:52 +0000

    FCC - timestamp - microservice

--- index.js
+++ index.js
@@ -18,12 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const invalidDate = (date) => date.toUTCString() === "Invalid Date";
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date", (req, res) => {
+  let date = new Date(req.params.date);
+  
+  if (invalidDate(date)) {
+    date = new Date(parseInt(req.params.date));
+  } 
 
+  if (invalidDate(date)) {
+    res.json({ error : "Invalid Date" })
+    return
+  }
+  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+    })
+})
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+});
+})
 
 
 // listen for requests :)
